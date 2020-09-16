fc9f45037023db4904190e30094dfc87c6cfd303
dawa93 <dawa93@naver.com>
2020-09-16 18:02:14 +0900

    finally completed all !!!!!!!

--- tree-map/tree-map.js
+++ tree-map/tree-map.js
@@ -36,74 +36,51 @@ var Tree = function(value) {
   this.children = [];
 };
 
-Tree.prototype.addChild = function(child) {
+Tree.prototype.addChild = function(value) {
   // your code here
-
-  //재귀를 사용하여, 새로운 노드를만든다.
+  // 자식중 같은 값이 없다면
+  //재귀(?)를 사용하여, 새로운 노드를만든다.
   // 그 노드를 상위 노드의 자식에 넣는다.
-  // if (!this.children.includes(child)) {
-  // }
-  let temp = new Tree(child);
+  // if (!this.children.includes(value)) {
+  let temp = new Tree(value);
   this.children.push(temp);
+  // }
 };
 
 Tree.prototype.map = function(callback) {
   // your code here
   // 새로운 함수의 리턴값을 루트 노드로 대입.
   let newTree = new Tree(callback(this.value));
-  let oldTree = new Tree(this.value);
-  // 기존 트리가 가지고 있던 노드들을 새로운 트리에 추가.
-  // 노드가 자식을 가지고 있다면,
-  function recursion(tree) {
-    // 자식이 존재 하지않는다면, 패스
-    if (oldTree.children.length === 0) {
-      return newTree;
-    }
-    // 반면에, 자식이 존재한다면
-    if (oldTree.children.length > 0) {
-      //반복문을 사용해서,
-      for (let i = 0; i < tree.children.length; i++) {
-        //새로운 트리에다가 map을 적용한 밸류값을 대입
-        newTree.addChild(tree.children[i].value);
-        // 만약 방금 보았던 자식에게도 자식이 있을경우엔
-        if (tree.children[i].children.length > 0) {
-          // 방금했던것을 다시 실행
-          recursion(tree.children[i].children);
-        }
-        // newTree.children[i] = this.children[i].map(callback);
-      }
+
+  if (this.children.length === 0) {
+    return newTree;
+  }
+  // 해야 할것 : 기존 트리가 가지고 있던 노드들 2배하고, 새로운 트리에 추가하고, .
+  // 자식이 존재한다면. 자식들의 값을 바꾸자.
+  if (this.children.length > 0) {
+    // 반복문을 사용해서, 모든 자식들을 순회한다.
+    for (let i = 0; i < this.children.length; i++) {
+      //새로운 트리에다가 map을 적용한 밸류값을 대입
+      // let doubledVal = callback(this.children[i].value);
+      // newTree.addChild(doubledVal);
+      newTree.children.push(this.children[i].map(callback));
     }
   }
-
-  recursion(newTree);
   return newTree;
 };
 
-let a = new Tree(1);
+// let a = new Tree(1);
 
-a.addChild(2);
-a.addChild(3);
-a.children[0].addChild(4);
-a.children[0].addChild(5);
-a.children[1].addChild(6);
+// a.addChild(2);
+// a.addChild(3);
+// a.children[0].addChild(4);
+// a.children[0].addChild(5);
+// a.children[1].addChild(6);
 
-let double = function(el) {
-  return el * 2;
-};
+// let double = function(el) {
+//   return el * 2;
+// };
 
-let b = a.map(double);
+// let b = a.map(double);
 
 module.exports = Tree;
-// if (this.children.length > 0) {
-//   //반복문을 사용해서,
-//   for (let i = 0; i < this.children.length; i++) {
-//     //새로운 트리에다가 map을 적용한 밸류값을 대입
-//     newTree.addChild(this.children[i].value);
-//     // 만약 방금 보았던 자식에게도 자식이 있을경우엔
-//     if(this.children[i].children){
-//       // 방금했던것을 다시 실행
-
-//     }
-//     // newTree.children[i] = this.children[i].map(callback);
-//   }
-// }
